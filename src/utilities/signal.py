9efb7599a4fc001ea92019987cd44da9eff320cc
Tymon Marek <tymon.marek@icloud.com>
2026-01-02 11:33:08 +0000

    fix: make signals less verbose and fix emit logic
    Signals are now less verbose, since they are used too often to have a log every time they are fired, even causing performance drops at times. Additionally, Signal.emit() logic is now simplified by being an async function instead of returning a Future.

--- src/utilities/signal.py
+++ src/utilities/signal.py
@@ -6,7 +6,6 @@ from typing import Any
 
 import asyncio
 
-
 import logging
 
 
@@ -28,9 +27,6 @@ class Connection[**P]:
 
     async def fire(self, *args: P.args, **kwargs: P.kwargs) -> None:
         if self.callback is not None:
-            self.logger.debug(
-                f"Firing callback {self.callback} with args={args}, kwargs={kwargs}"
-            )
             await self.callback(*args, **kwargs)
 
     @property
@@ -38,42 +34,33 @@ class Connection[**P]:
         return self in self.signal.connections
 
     def disconnect(self) -> None:
-        self.logger.debug("Disconnecting connection.")
         self.signal.disconnect(self)
 
 
 class Signal[**P]:
     def __init__(self) -> None:
-        self.logger = logging.getLogger(__name__ + ".Signal")
+        self._logger = logging.getLogger(__name__ + ".Signal")
         self.connections: list[Connection[P]] = []
 
     def connect(self, callback: Callable[P, Awaitable[Any]]) -> Connection[P]:
-        self.logger.debug(f"Connecting callback: {callback}")
+        self._logger.debug(f"Connecting callback: {callback}")
         connection = Connection[P](self, callback)
         self.connections.append(connection)
         return connection
 
     def disconnect(self, connection: Connection[P]) -> None:
         if not connection in self.connections:
-            self.logger.error("Connection not found during disconnect.")
+            self._logger.error("Connection not found during disconnect.")
             raise ConnectionNotFoundException()
-        self.logger.debug("Disconnecting connection.")
+        self._logger.debug("Disconnecting connection.")
         self.connections.remove(connection)
 
-    def emit(self, *args: P.args, **kwargs: P.kwargs) -> Future[list[None]]:
-        if not self.connections:
-            future: Future[list[None]] = asyncio.get_event_loop().create_future()
-            future.set_result([])
-            return future
-        self.logger.debug(
-            f"Emitting signal to {len(self.connections)} connections with args={args}, kwargs={kwargs}"
-        )
-        return asyncio.gather(
+    async def emit(self, *args: P.args, **kwargs: P.kwargs) -> None:
+        await asyncio.gather(
             *(connection.fire(*args, **kwargs) for connection in self.connections)
         )
 
     async def wait(self) -> None:
-        self.logger.debug("Waiting for signal emission.")
         future: Future[None] = asyncio.get_event_loop().create_future()
 
         async def _on_emit(*args: P.args, **kwargs: P.kwargs) -> None:
@@ -90,7 +77,6 @@ class Signal[**P]:
 
         async def wrapper(*args: P.args, **kwargs: P.kwargs) -> None:
             if connection:
-                self.logger.debug("Disconnecting after one emission.")
                 connection.disconnect()
             await callback(*args, **kwargs)
 
