49f84e9feb142523897811a37765a473da746f6d
Tymon Marek <tymon.marek@icloud.com>
2026-01-03 12:01:37 +0000

    refactor(prefabs): update prefabs to use class attributes instead of __init__
    This change allows the use of custom arguments for Prefabs, such as passing text to a TextPrefab instead of changing its properties after creation

--- src/justkeepswimming/prefabs/cameras.py
+++ src/justkeepswimming/prefabs/cameras.py
@@ -2,18 +2,22 @@ from justkeepswimming.components.sizing import SceneSizeConstraint
 from justkeepswimming.components.physics import Transform
 from justkeepswimming.components.render import Camera, MainCamera
 
+from justkeepswimming.ecs import Component, System
 from justkeepswimming.systems.render import RenderSystem
 from justkeepswimming.systems.cameras import CameraSystem
 
 from justkeepswimming.utilities.prefab import Prefab
 from justkeepswimming.systems.sizing import SceneSizeConstraintSystem
 
-main_camera_prefab = Prefab(
-    components=[
+class MainCameraPrefab(Prefab):
+    components: list[Component] = [
         Camera(),
         Transform(),
         MainCamera(),
         SceneSizeConstraint(),
-    ],
-    systems=[RenderSystem, CameraSystem, SceneSizeConstraintSystem],
-)
+    ]
+    systems: list[type[System]] = [
+        RenderSystem,
+        CameraSystem,
+        SceneSizeConstraintSystem,
+    ]
